95b4ef65cb86221385eaf4d93b9a4438b4ab651c
MaximilianoRolon <maxirolon@gmail.com>
2018-08-21 19:57:03 -0300

    "First part"

---
+++ app/src/main/java/com/example/maxir/recyclerview/Contactos.java
@@ -0,0 +1,68 @@
+package com.example.maxir.recyclerview;
+
+public class Contactos {
+
+    private String fecha;
+    private String nombre;
+    private String telefono;
+    private String email;
+    private String descripcion;
+    private int foto;
+
+    public Contactos (String fecha, String nombre, String telefono, String email, String descripcion, int foto){
+        this.fecha = fecha;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+        this.descripcion = descripcion;
+        this.foto = foto;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+}
