c2e6cb36f1771a25dc46fb37796c4b96d2b714c8
MaximilianoRolon <maxirolon@gmail.com>
2018-08-23 00:11:17 -0300

    "First part"

---
+++ app/src/main/java/com/example/maxir/recyclerview/ContactoAdaptador.java
@@ -0,0 +1,58 @@
+package com.example.maxir.recyclerview;
+
+import android.media.Image;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ContactoAdaptador extends RecyclerView.Adapter<ContactoAdaptador.ContactoViewHolder>{
+
+    ArrayList<Contactos> contactos;
+
+    public ContactoAdaptador(ArrayList<Contactos> contactos){
+        this.contactos = contactos;
+    }
+
+    @NonNull
+    @Override
+    public ContactoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_contacto, parent, false);
+        return new ContactoViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactoViewHolder contactoViewHolder, int position) {
+        Contactos contacto = contactos.get(position);
+
+        contactoViewHolder.imgFoto.setImageResource(contacto.getFoto());
+        contactoViewHolder.tvNombreCV.setText(contacto.getNombre());
+        contactoViewHolder.tvTelefonoCV.setText(contacto.getTelefono());
+    }
+
+    @Override
+    public int getItemCount() { // Cantidad de elementos que contiene mi lista
+        return contactos.size();
+    }
+
+    public static class ContactoViewHolder extends RecyclerView.ViewHolder{
+
+        private ImageView imgFoto;
+        private TextView tvNombreCV;
+        private TextView tvTelefonoCV;
+
+
+        public ContactoViewHolder(View itemView) {
+            super(itemView);
+            imgFoto         = (ImageView) itemView.findViewById(R.id.imgFoto);
+            tvNombreCV      = (TextView) itemView.findViewById(R.id.tvNombreCV);
+            tvTelefonoCV    = (TextView) itemView.findViewById(R.id.tvTelefonoCV);
+        }
+    }
+
+}
--- app/src/main/java/com/example/maxir/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/maxir/recyclerview/MainActivity.java
@@ -3,11 +3,22 @@ package com.example.maxir.recyclerview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<Contactos> contactos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        contactos.add(new Contactos("15/7", "Vault Boy", "0761411", "fo76@opgg.com", "BOooyyy", R.drawable.vault_boy));
+        contactos.add(new Contactos("15/7", "Vault Boy", "0761411", "fo76@opgg.com", "BOooyyy", R.drawable.vault_boy));
+        contactos.add(new Contactos("15/7", "Vault Boy", "0761411", "fo76@opgg.com", "BOooyyy", R.drawable.vault_boy));
+        contactos.add(new Contactos("15/7", "Vault Boy", "0761411", "fo76@opgg.com", "BOooyyy", R.drawable.vault_boy));
+        contactos.add(new Contactos("15/7", "Vault Boy", "0761411", "fo76@opgg.com", "BOooyyy", R.drawable.vault_boy));
+
+
+
     }
 }
